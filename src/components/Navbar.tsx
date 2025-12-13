a1a8211145ca942cb9e5507438e017d7a96dbc2b
westeegarden <160755607+westeeg@users.noreply.github.com>
2025-12-13 12:12:38 -0500

    Spaced and styled navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,11 @@ export default function Navbar() {
 return (
     <nav className="flex justify-between items-start px-8 py-4 shadow bg-white">
         <h1 className="site-title">Wesley Teegarden</h1>
-        <div className="space-x-6 text-lg">
-            <a href="#projects" className="hover:underline">Projects</a>
-            <a href="#contact" className="hover:underline">Contact</a>
-            <a href='#resume' className="hover:underline">Resume</a>
-            <a href='#about' className="hover:underline">About</a>
+        <div className="nav-links">
+            <a href="#projects">Projects</a>
+            <a href="#contact">Contact</a>
+            <a href='#resume'>Resume</a>
+            <a href='#about'>About</a>
         </div>
     </nav>
 );
